0bb0accf8d633b2e21179fb076c4aa3fd9ab373b
Valentyn-Okh <v.okhotskiy01@.com>
2025-12-22 13:09:56 +0200

    style NewContact

--- src/pages/NewContact/NewContact.jsx
+++ src/pages/NewContact/NewContact.jsx
@@ -30,64 +30,92 @@ export default function NewContact(){
   }
 
   return(
-    <main className="shadow bg-white container rounded mt-4 addPage">
-          <Formik initialValues={initialValues} validationSchema={contactValidationSchema} onSubmit={handleSubmit}>
-            {({ isSubmitting }) => (
-              <Form>
-                <h1 className="text-center">Add new contact</h1>
-                <hr />
-                <div className='m-4'>
-                  <label htmlFor="firstName">First name</label>
-                  <Field className='form-control fs-5' type='text' name='firstName' id='firstName'/>
-                  <ErrorMessage name='firstName' component='p' className='text-danger position-absolute'/>
-                </div>
-                <div className='m-4'>
-                  <label htmlFor="lastName">Last name</label>
-                  <Field className='form-control fs-5' type='text' name='lastName' id='lastName'/>
-                  <ErrorMessage name='lastName' component='p' className='text-danger position-absolute'/>
-                </div>
-                <div className='m-4'>
-                  <label htmlFor="phone">Phone</label>
-                  <Field className='form-control fs-5' type='text' name='phone' id='phone'/>
-                  <ErrorMessage name='phone' component='p' className='text-danger position-absolute'/>
-                </div>
-                <div className='m-4'>
-                  <label htmlFor="email">Email</label>
-                  <Field className='form-control fs-5' type='text' name='email' id='email'/>
-                  <ErrorMessage name='email' component='p' className='text-danger position-absolute'/>
-                </div>
-                <div className='m-4'>
-                  <label htmlFor="avatar">Avatar</label>
-                  <Field className='form-control fs-5' type='number' min={0} max={99} name='avatar' id='avatar'/>
-                  <ErrorMessage name='avatar' component='p' className='text-danger position-absolute'/>
-                </div>
-                <div className='m-4'>
-                  <label htmlFor="gender">Gender</label>
-                  <Field className='form-control fs-5' as='select' name='gender' id='gender'>
-                    <option value="">Choose gender</option>
-                    <option value="men">Men</option>
-                    <option value="women">Women</option>
-                  </Field>
-                  <ErrorMessage name='gender' component='p' className='text-danger position-absolute'/>
-                </div>
-                <div className='m-4'>
-                  <label htmlFor="status">Status</label>
-                  <Field className='form-control fs-5' as='select' name='status' id='status'>
-                    <option value="">Choose status</option>
-                    {Object.keys(contactStatuses).map((status, index) => (
-                      <option style={{backgroundColor: contactStatuses[status].bg}} key={index} value={status}>{status}</option>
-                    ))}
-                  </Field>
-                  <ErrorMessage name='status' component='p' className='text-danger position-absolute'/>
-                </div>
-                <div className='m-4'>
-                  <label className='form-check-label fs-5' htmlFor="favorite">Favorite</label>
-                  <Field className='form-check-input m-1 fs-4' type='checkbox' name='favorite' id='favorite'/>
-                </div>
-                <button type='submit' className='btn btn-primary btn-lg form-control' disabled={isSubmitting}>Save</button>
-              </Form>
-            )}
-          </Formik>
-    </main>
+    <div className="page-wrapper">
+  <main className="shadow-lg bg-white container addPage">
+    <Formik 
+      initialValues={initialValues} 
+      validationSchema={contactValidationSchema} 
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <h1 className="text-center form-title">New Contact</h1>
+          
+          <div className="row g-3">
+            <div className='col-md-6'>
+              <label className="form-label fw-bold" htmlFor="firstName">First name</label>
+              <Field className='form-control custom-input' name='firstName' id='firstName' placeholder="e.g. Alex"/>
+              <ErrorMessage name='firstName' component='div' className='text-danger error-msg'/>
+            </div>
+
+            <div className='col-md-6'>
+              <label className="form-label fw-bold" htmlFor="lastName">Last name</label>
+              <Field className='form-control custom-input' name='lastName' id='lastName' placeholder="e.g. Smith"/>
+              <ErrorMessage name='lastName' component='div' className='text-danger error-msg'/>
+            </div>
+
+            <div className='col-md-6 mt-4'>
+              <label className="form-label fw-bold" htmlFor="phone">Phone</label>
+              <Field className='form-control custom-input' name='phone' id='phone' placeholder="+380..."/>
+              <ErrorMessage name='phone' component='div' className='text-danger error-msg'/>
+            </div>
+
+            <div className='col-md-6 mt-4'>
+              <label className="form-label fw-bold" htmlFor="email">Email</label>
+              <Field className='form-control custom-input' type='email' name='email' id='email' placeholder="mail@example.com"/>
+              <ErrorMessage name='email' component='div' className='text-danger error-msg'/>
+            </div>
+
+            <div className='col-md-6 mt-4'>
+              <label className="form-label fw-bold" htmlFor="avatar">Avatar (0-99)</label>
+              <Field className='form-control custom-input' type='number' name='avatar' id='avatar'/>
+              <ErrorMessage name='avatar' component='div' className='text-danger error-msg'/>
+            </div>
+
+            <div className='col-md-6 mt-4'>
+              <label className="form-label fw-bold" htmlFor="gender">Gender</label>
+              <Field className='form-select custom-input' as='select' name='gender' id='gender'>
+                <option value="">Choose...</option>
+                <option value="men">Men</option>
+                <option value="women">Women</option>
+              </Field>
+              <ErrorMessage name='gender' component='div' className='text-danger error-msg'/>
+            </div>
+
+            <div className='col-12 mt-4'>
+              <label className="form-label fw-bold" htmlFor="status">Status</label>
+              <Field className='form-select custom-input' as='select' name='status' id='status'>
+                <option value="">Select status</option>
+                {Object.keys(contactStatuses).map((status, index) => (
+                  <option key={index} value={status}>{status}</option>
+                ))}
+              </Field>
+              <ErrorMessage name='status' component='div' className='text-danger error-msg'/>
+            </div>
+
+            <div className='col-12 mt-4'>
+              <div className="favorite-wrapper">
+                <Field className='form-check-input mt-0' type='checkbox' name='favorite' id='favorite'/>
+                <label className='form-check-label ms-3 fw-bold' htmlFor="favorite">
+                  Add to Favorites ⭐
+                </label>
+              </div>
+            </div>
+
+            <div className="col-12 mt-5">
+              <button 
+                type='submit' 
+                className='btn btn-primary btn-lg w-100 btn-save' 
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? 'Saving...' : 'Save Contact'}
+              </button>
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  </main>
+</div>
   )
 }
