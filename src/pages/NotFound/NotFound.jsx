c44b48214a7564fa073f887fe4ce89761a776517
Valentyn-Okh <v.okhotskiy01@.com>
2025-12-22 12:27:40 +0200

    NotFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,19 +1,43 @@
-import { useState } from "react";
+import './NotFound.scss';
+
 import { Link } from "react-router-dom";
 
 export default function NotFound(){
-  const [count, setCount] = useState(0)
-
-  const addone = () => {
-    setCount(count + 1)
-  }
   return(
-    <div className="shadow bg-white container rounded mt-4">
-      <h1>NotFound 404</h1>
-      <Link to='/'><button>Go home</button></Link>
+    <div className="container">
+      <h1 className="sorry">SORRY</h1>
+      <h2 className="not-found">PAGE NOT FOUND</h2>
 
-      <p>You clicked {count} times</p>
-      <button onClick={addone}>Click me</button>
+      <div className="scene">
+        <div className="ice-hole"></div>
+            
+        <div className="penguin">
+            <div className="body">
+              <div className="white-belly"></div>
+              <div className="eyes">
+                <div className="eye left"></div>
+                <div className="eye right"></div>
+              </div>
+              <div className="beak"></div>
+            </div>
+            <div className="arm left-arm"></div>
+            <div className="arm right-arm"></div>
+            <div className="feet">
+              <div className="foot left-foot"></div>
+              <div className="foot right-foot"></div>
+            </div>
+        </div>
+
+        <div className="fishing-rod">
+          <div className="line"></div>
+          <div className="error-sign">
+            <p>ERROR</p>
+            <span>404</span>
+          </div>
+        </div>
+      </div>
+      <Link to='/'><button className='btn-home'>Go home</button></Link>
     </div>
   )
 }
+
